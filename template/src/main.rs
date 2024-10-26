ddd3729ce805031b9ae8af1856d66a035097a7d8
David Axelsson <davax320@student.liu.se>
2024-10-26 14:51:36 +0200

    Initial setup for AOC 2024

---
+++ template/src/main.rs
@@ -0,0 +1,13 @@
+// Change day_01 to the correct day
+use day_01::{part1, part2};
+fn main() {
+    // Output results for both parts
+    println!(
+        "Part 1:\n  {}",
+        part1::task(include_str!("../input.txt")).unwrap_or("No solution found".to_string())
+    );
+    println!(
+        "Part 2:\n  {}",
+        part2::task(include_str!("../input.txt")).unwrap_or("No solution found".to_string())
+    );
+}
---
+++ template/src/part2.rs
@@ -0,0 +1,16 @@
+pub fn task(_input: &str) -> Option<String> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_task() {
+        let input = include_str!("../example.txt");
+        let result = task(input);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), "");
+    }
+}
