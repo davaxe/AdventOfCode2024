d3d686d7946b7071d0c315755bbad7ff59fc7b95
David Axelsson <davidaxelsson5@gmail.com>
2024-11-29 15:41:02 +0100

    Fix formatting

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -4,10 +4,12 @@ fn main() {
     // Output results for both parts
     println!(
         "Part 1:\n  {}",
-        part1::task(include_str!("../input.txt")).unwrap_or("No solution found".to_string())
+        part1::task(include_str!("../input.txt"))
+            .unwrap_or("No solution found".to_string())
     );
     println!(
         "Part 2:\n  {}",
-        part2::task(include_str!("../input.txt")).unwrap_or("No solution found".to_string())
+        part2::task(include_str!("../input.txt"))
+            .unwrap_or("No solution found".to_string())
     );
 }
--- template/benches/benchmarks.rs
+++ template/benches/benchmarks.rs
@@ -5,18 +5,22 @@ fn part1_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("part1");
 
     let input = include_str!("../input.txt");
-    group.bench_with_input(BenchmarkId::new("part1", "input"), &input, |b, input| {
-        b.iter(|| part1::task(input))
-    });
+    group.bench_with_input(
+        BenchmarkId::new("part1", "input"),
+        &input,
+        |b, input| b.iter(|| part1::task(input)),
+    );
 }
 
 fn part2_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("part2");
 
     let input = include_str!("../input.txt");
-    group.bench_with_input(BenchmarkId::new("part2", "input"), &input, |b, input| {
-        b.iter(|| part2::task(input))
-    });
+    group.bench_with_input(
+        BenchmarkId::new("part2", "input"),
+        &input,
+        |b, input| b.iter(|| part2::task(input)),
+    );
 }
 
 criterion_group!(benches, part1_benchmark, part2_benchmark);
