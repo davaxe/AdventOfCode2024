aacd3dc6874399cc1cd45c987d99c4875bd87418
David Axelsson <davidaxelsson5@gmail.com>
2024-12-01 12:41:54 +0100

    Day 1 complete

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,6 +1,30 @@
+/// Processes the input and returns the result as a string.
+///
+/// # Panics
+///
+/// This function will panic if the input lines do not contain at least two
+/// whitespace-separated numbers.
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+    input.lines().for_each(|line| {
+        let mut numbers = line.split_whitespace();
+        left.push(numbers.next().unwrap().parse::<i32>().unwrap());
+        right.push(numbers.next().unwrap().parse::<i32>().unwrap());
+    });
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    Some(
+        right
+            .iter()
+            .zip(left.iter())
+            .map(|(a, b)| (a - b).abs())
+            .sum::<i32>()
+            .to_string(),
+    )
 }
 
 #[cfg(test)]
@@ -8,11 +32,10 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "11");
     }
 }
--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,6 +1,25 @@
+/// Processes the input and returns the result as a string.
+///
+/// # Panics
+///
+/// This function will panic if the input lines do not contain at least two
+/// whitespace-separated numbers.
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut left = vec![];
+    let mut right = vec![];
+    input.lines().for_each(|line| {
+        let mut numbers = line.split_whitespace();
+        left.push(numbers.next().unwrap().parse::<usize>().unwrap());
+        right.push(numbers.next().unwrap().parse::<usize>().unwrap());
+    });
+
+    Some(
+        left.iter()
+            .map(|n| right.iter().filter(|&m| m == n).count() * n)
+            .sum::<usize>()
+            .to_string(),
+    )
 }
 
 #[cfg(test)]
@@ -8,11 +27,10 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "31");
     }
 }
