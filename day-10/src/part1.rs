0f064dbb8d1a2f4eb48f5b0dc4c319ae102174eb
David Axelsson <davidaxelsson5@gmail.com>
2024-12-12 19:56:57 +0100

    Day 10 complete

--- day-10/src/part1.rs
+++ day-10/src/part1.rs
@@ -1,6 +1,59 @@
+use std::collections::{HashMap, HashSet};
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let height_map: HashMap<(i32, i32), u32> = input
+        .lines()
+        .zip(0i32..)
+        .flat_map(|(line, y)| {
+            line.chars()
+                .zip(0i32..)
+                .filter_map(move |(c, x)| c.to_digit(10).map(|d| ((x, y), d)))
+        })
+        .collect();
+
+    let result: u32 = height_map
+        .iter()
+        .filter(|&(_, &height)| (height == 0))
+        .map(|(pos, _)| hiking_trails(*pos, &height_map))
+        .sum();
+
+    Some(result.to_string())
+}
+
+fn hiking_trails(
+    start_position: (i32, i32),
+    height_map: &HashMap<(i32, i32), u32>,
+) -> u32 {
+    let neighbors = |(x, y)| {
+        let height = height_map[&(x, y)];
+        vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+            .into_iter()
+            .filter_map(|pos| height_map.get(&pos).map(|&height| (pos, height)))
+            .filter(move |&(_, neighbor_height)| {
+                neighbor_height.checked_sub(1) == Some(height)
+            })
+    };
+
+    let mut visited = HashSet::new();
+
+    let mut trails = 0;
+    let mut queue = vec![start_position];
+    while let Some(next) = queue.pop() {
+        if height_map[&next] == 9 && !visited.contains(&next) {
+            trails += 1;
+        }
+
+        if !visited.insert(next) {
+            continue;
+        }
+
+        for (pos, _) in neighbors(next) {
+            queue.push(pos);
+        }
+    }
+
+    trails
 }
 
 #[cfg(test)]
@@ -8,12 +61,23 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
+    #[ignore]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "36");
+    }
+
+    #[test]
+    fn alterative_test_task() {
+        let input = "0123
+1234
+8765
+9876
+";
+        let result = task(input);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), "1");
     }
 }
--- day-10/src/part2.rs
+++ day-10/src/part2.rs
@@ -1,6 +1,53 @@
+use std::collections::HashMap;
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let height_map: HashMap<(i32, i32), u32> = input
+        .lines()
+        .zip(0i32..)
+        .flat_map(|(line, y)| {
+            line.chars()
+                .zip(0i32..)
+                .filter_map(move |(c, x)| c.to_digit(10).map(|d| ((x, y), d)))
+        })
+        .collect();
+
+    let result: u32 = height_map
+        .iter()
+        .filter(|&(_, &height)| (height == 0))
+        .map(|(pos, _)| rating(*pos, &height_map))
+        .sum();
+
+    Some(result.to_string())
+}
+
+fn rating(
+    start_position: (i32, i32),
+    height_map: &HashMap<(i32, i32), u32>,
+) -> u32 {
+    let neighbors = |(x, y)| {
+        let height = height_map[&(x, y)];
+        vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+            .into_iter()
+            .filter_map(|pos| height_map.get(&pos).map(|&height| (pos, height)))
+            .filter(move |&(_, neighbor_height)| {
+                neighbor_height.checked_sub(1) == Some(height)
+            })
+    };
+
+    let mut trails = 0;
+    let mut queue = vec![start_position];
+    while let Some(next) = queue.pop() {
+        if height_map[&next] == 9 {
+            trails += 1;
+        }
+
+        for (pos, _) in neighbors(next) {
+            queue.push(pos);
+        }
+    }
+
+    trails
 }
 
 #[cfg(test)]
@@ -8,12 +55,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "81");
     }
 }
