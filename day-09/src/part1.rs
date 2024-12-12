f0f2a5ca6b0a1da9cf48eb8121ac1e711aad8d38
David Axelsson <davidaxelsson5@gmail.com>
2024-12-12 18:11:48 +0100

    Day 9 complete

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -1,6 +1,64 @@
+use std::iter;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum MemorySlot {
+    Free,
+    Used(usize),
+}
+
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut disk_map = input
+        .char_indices()
+        .filter_map(|(i, c)| match c.to_digit(10) {
+            Some(x) if i % 2 == 0 => {
+                Some(iter::repeat(MemorySlot::Used(i / 2)).take(x as usize))
+            }
+            Some(x) if i % 2 != 0 => {
+                Some(iter::repeat(MemorySlot::Free).take(x as usize))
+            }
+            _ => None,
+        })
+        .flatten()
+        .collect::<Vec<_>>();
+
+    let mut move_index = next_move_index(&disk_map, disk_map.len())?;
+
+    for i in 0..disk_map.len() {
+        if let MemorySlot::Used(_) = disk_map[i] {
+            continue;
+        }
+
+        if move_index < i {
+            break;
+        }
+
+        disk_map.swap(move_index, i);
+        move_index = next_move_index(&disk_map, move_index)?;
+    }
+
+    let result: usize = disk_map
+        .iter()
+        .enumerate()
+        .filter_map(|(i, x)| {
+            if let MemorySlot::Used(id) = x {
+                Some(id * i)
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    Some(result.to_string())
+}
+
+fn next_move_index(
+    disk_map: &[MemorySlot],
+    move_index: usize,
+) -> Option<usize> {
+    (0..move_index)
+        .rev()
+        .find(|&i| matches!(disk_map[i], MemorySlot::Used(_)))
 }
 
 #[cfg(test)]
@@ -8,12 +66,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "1928");
     }
 }
