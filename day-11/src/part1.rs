6bd8f416dd856dc18d1642260712f732d47ac1a0
David Axelsson <davidaxelsson5@gmail.com>
2024-12-13 13:27:56 +0100

    Complete part 1 of day 11
    Skipping part 2 for now

--- day-11/src/part1.rs
+++ day-11/src/part1.rs
@@ -1,6 +1,27 @@
 #[must_use]
-pub fn task(_input: &str) -> Option<String> {
-    None
+pub fn task(input: &str) -> Option<String> {
+    let mut stones = input
+        .split_ascii_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect::<Vec<u64>>();
+
+    for _ in 0..25 {
+        let mut new = Vec::with_capacity(stones.len());
+        for stone in stones {
+            match format!("{stone}").as_str() {
+                "0" => new.push(1),
+                n if n.len() % 2 == 0 => {
+                    let first_halp = n[..n.len() / 2].parse::<u64>().ok()?;
+                    let second_half = n[n.len() / 2..].parse::<u64>().ok()?;
+                    new.push(first_halp);
+                    new.push(second_half);
+                }
+                n => new.push(n.parse::<u64>().ok()? * 2024),
+            }
+        }
+        stones = new;
+    }
+    Some(stones.len().to_string())
 }
 
 #[cfg(test)]
@@ -8,12 +29,10 @@ mod tests {
     use super::*;
 
     #[test]
-    // TODO: Implement test, and remove ignore.
-    #[ignore = "Not implemented"]
     fn test_task() {
         let input = include_str!("../example.txt");
         let result = task(input);
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), "");
+        assert_eq!(result.unwrap(), "55312");
     }
 }
